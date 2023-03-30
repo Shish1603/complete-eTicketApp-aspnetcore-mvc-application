fdbc54e7ec0596317f7d9feba6678e0873053315
shishir_Rao <shishir.rao3@gmail.com>
2023-03-30 00:46:04 -0400

    Video 8.Add Actor Model

---
+++ Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketApp.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
