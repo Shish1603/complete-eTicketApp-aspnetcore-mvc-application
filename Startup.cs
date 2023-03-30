436a51f7a4c51b72827e88098335b6d0a15b7542
shishir_Rao <shishir.rao3@gmail.com>
2023-03-30 01:12:44 -0400

    P12->Added the ApplicationDbContext class

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using eTicketApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace eTicketApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContext configuration
+            services.AddDbContext<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
 
