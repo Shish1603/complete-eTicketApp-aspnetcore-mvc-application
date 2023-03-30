910aa4e490780903fdfb1eff327ea6e5d1885d3b
shishir_Rao <shishir.rao3@gmail.com>
2023-03-29 20:33:16 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTicketApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
