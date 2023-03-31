86ca4b79905f2ead3a353af87d8edb28ca4563f1
shishir_Rao <shishir.rao3@gmail.com>
2023-03-31 11:51:12 -0400

    P.18-21->Adding controllers for Actor Movie Cinema and Producer models

---
+++ Controllers/CinemaController.cs
@@ -0,0 +1,29 @@
+﻿using eTicketApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketApp.Controllers
+{
+    public class CinemaController : Controller
+    {
+        //inject AppDbContext
+        private readonly ApplicationDbContext _context;
+
+        public CinemaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //to fetch data asynchronously using async
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
